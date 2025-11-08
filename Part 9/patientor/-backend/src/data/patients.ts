e04f84a8671befadfed471c248eb43bd364873a4
Gladys Gopolang Masia <gladysgops@gmail.com>
2025-11-08 16:25:49 +0200

    Create patients.ts

---
+++ Part 9/patientor/-backend/src/data/patients.ts
@@ -0,0 +1,20 @@
+import { Patient } from '../types';
+
+const patients: Patient[] = [
+  {
+    id: "1",
+    name: "John Doe",
+    dateOfBirth: "1990-01-01",
+    occupation: "Engineer",
+    entries: []
+  },
+  {
+    id: "2",
+    name: "Jane Smith",
+    dateOfBirth: "1985-03-15",
+    occupation: "Designer",
+    entries: []
+  }
+];
+
+export default patients;
